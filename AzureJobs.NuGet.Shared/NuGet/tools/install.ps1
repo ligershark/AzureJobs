087d2629fe297b0983f3387459f9898e501a055a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-05-23 17:16:51 -0700

    start creating shared nuget pkg, not done. related to #2.

---
+++ AzureJobs.NuGet.Shared/NuGet/tools/install.ps1
@@ -0,0 +1,42 @@
+﻿param($rootPath, $toolsPath, $package, $project)
+
+
+if((Get-Module azure-jobs)){
+    Remove-Module azure-jobs
+}
+Import-Module (Join-Path -Path ($toolsPath) -ChildPath 'azure-jobs.psm1')
+
+#########################
+# Start of script here
+#########################
+<#
+$projDir = (Get-Item $project.FullName).Directory.FullName
+$jobsPropsFile = Join-Path $projDir 'azurejobs.props'
+
+$jobsProps = $null
+if(!(Test-Path $jobsPropsFile)){
+    # create the file since it is not there
+    $jobsProps = New-Project -filePath $jobsPropsFile
+    $project.ProjectItems.AddFromFile($jobsPropsFile) | Out-Null
+}
+
+if(!$jobsProps){
+    $jobsProps = [Microsoft.Build.Construction.ProjectRootElement]::Open($jobsPropsFile)
+}
+
+# Before modifying the project save everything so that nothing is lost
+$DTE.ExecuteCommand("File.SaveAll")
+
+CheckoutIfUnderScc -filePath $jobsPropsFile -project $project
+EnsureFileIsWriteable -filePath $jobsPropsFile
+
+$pgLabel = 'ls-azurejobs-imageopt'
+RemoveExistingKnownPropertyGroups -projectRootElement $jobsProps -importLabel $pgLabel
+$relPathToToolsFolder = ComputeRelativePathToTargetsFile -startPath (Get-Item $project.FullName) -targetPath (Get-Item ("{0}\tools\" -f $rootPath))
+
+$propertyGroup = $jobsProps.AddPropertyGroup()
+$propertyGroup.Label = $pgLabel
+$propertyGroup.AddProperty('ls-AzureImageOptimizerToolsPath', ('$([System.IO.Path]::GetFullPath( $(MSBuildProjectDirectory)\{0}\))') -f $relPathToToolsFolder);
+
+$jobsProps.Save()
+#>
