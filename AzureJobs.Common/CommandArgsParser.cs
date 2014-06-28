e868e47e2a202f1ddad3e8bdc97ffc41cbb99591
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-06-28 15:15:25 -0700

    added class to parse cmd line args, and test project. related to #22

---
+++ AzureJobs.Common/CommandArgsParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureJobs.Common {
+    public class CommandArgsParser {
+
+        // --file one.txt --name "azure image optimizer" --color Green
+        // --help or /?
+        public IDictionary<string, string> ParseArgs(string[] args) {
+            IDictionary<string, string> result = new Dictionary<string, string>();
+
+            string currentKey = null;
+            StringBuilder currentValue = new StringBuilder();
+            foreach (string argPart in args) {
+                string trimmedArgPart = argPart;
+
+                if (trimmedArgPart != null) { trimmedArgPart = argPart.Trim(); }
+
+                if (trimmedArgPart.StartsWith("-")) {
+
+                    if(!string.IsNullOrEmpty(currentKey)){
+                        // apply current key / value to the dictionary
+                        string value = currentValue.ToString().Trim();
+                        if(string.IsNullOrEmpty(value)){
+                            value = true.ToString();
+                        }
+
+                        result[currentKey] = value;
+                        currentValue = new StringBuilder();
+                    }
+                                       
+                    currentKey = trimmedArgPart;
+                }
+                else if(trimmedArgPart.Equals("/?")){
+                    currentKey = "--help";
+                }
+                else {
+                    currentValue.Append(argPart);
+                    currentValue.Append(" ");
+                }
+            }
+
+            if(!string.IsNullOrEmpty(currentKey)){
+                // apply current key / value to the dictionary
+                string value = currentValue.ToString().Trim(); ;
+                if(string.IsNullOrEmpty(value)){
+                    value = true.ToString();
+                }
+
+                result.Add(currentKey, value);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AzureJobsTests/CommandArgsParserTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using AzureJobs.Common;
+
+namespace AzureJobsTests {
+    [TestClass]
+    public class CommandArgsParserTests {
+        [TestClass]
+        public class TheParseArgsMethod {
+            [TestMethod]
+            public void ReturnsTheValues() {
+                string[] args = new string[8];
+                args[0] = "--logFile";
+                args[1] = "one.txt";
+                args[2] = "--name";
+                args[3] = "Azure";
+                args[4] = "Image";
+                args[5] = "Optimizer";
+                args[6] = "--color";
+                args[7] = "Green";
+
+                IDictionary<string, string> result = new CommandArgsParser().ParseArgs(args);
+                Assert.AreEqual(3, result.Count);
+                Assert.IsTrue(result.Keys.Contains("--logFile"));
+                Assert.IsTrue(result.Keys.Contains("--name"));
+                Assert.IsTrue(result.Keys.Contains("--color"));
+                Assert.AreEqual("one.txt", result["--logFile"]);
+                Assert.AreEqual("Azure Image Optimizer", result["--name"]);
+                Assert.AreEqual("Green", result["--color"]);
+            }
+
+            [TestMethod]
+            public void HandlesHelpAsShortName() {
+                string[] args = new string[1];
+                args[0] = "/?";
+
+                IDictionary<string, string> result = new CommandArgsParser().ParseArgs(args);
+                Assert.IsTrue(result.Keys.Contains("--help"));
+                Assert.AreEqual(true.ToString(), result["--help"]);
+                string foo = "bar";
+            }
+        }
+    }
+}
