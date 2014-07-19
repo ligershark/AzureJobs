bf3a93e2d6302ec8b9ce94eabe386124d7611885
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-07-19 12:14:47 -0700

    Refactoring, adding test cases and fixing bug in csv writing. related to #34 and #28

---
+++ AzureJobs.Common/CsvHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace AzureJobs.Common {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CsvHelper {
+        public string Escape(string inputToClean) {
+            const string DoubleQuote = "\"";
+            const string EscapedItemFormat = "{0}{1}{0}";
+            const string ItemsToCleanFor = ",\r\n\"";
+            if (inputToClean.IndexOfAny(ItemsToCleanFor.ToCharArray()) > -1) {
+                //wrap the input string in double quotes, while replacing any double quotes in the string with 2 double quotes
+                return string.Format(EscapedItemFormat, DoubleQuote,
+                                                        inputToClean.Replace(DoubleQuote, DoubleQuote + DoubleQuote));
+            }
+
+            return inputToClean;
+        }
+    }
+}
---
+++ AzureJobsTests/LoggerTests.cs
@@ -0,0 +1,66 @@
+﻿using AzureJobs.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureJobsTests {
+    [TestClass]
+    public class CsvHelperTests {
+        [TestClass]
+        public class TheCleanMessageForCsvMethod {
+
+            [TestMethod]
+            public void DoesNotModifyNormalCharatersTest1() {
+                string str = "no escaping here";
+                string escapedStr = new CsvHelper().Escape(str);
+
+                Assert.AreEqual(str, escapedStr);
+            }
+            [TestMethod]
+            public void EscapesNewLineCharacters1() {
+                string str =
+@"foo bar
+New line";
+                string escapedStr = new CsvHelper().Escape(str);
+
+                string expetedStr = "\"foo bar\r\nNew line\"";
+
+                Assert.AreEqual(expetedStr, escapedStr);
+            }
+
+            [TestMethod]
+            public void EscapesNewLineCharaters2() {
+                string str = "foo bar\n new line";
+                string escapedStr = new CsvHelper().Escape(str);
+                string expectedStr = "\"foo bar\n new line\"";
+                Assert.AreEqual(expectedStr, escapedStr);
+            }
+
+            [TestMethod]
+            public void EscapesNewLineCharaters3() {
+                string str = "foo bar\r new line";
+                string escapedStr = new CsvHelper().Escape(str);
+                string expectedStr = "\"foo bar\r new line\"";
+                Assert.AreEqual(expectedStr, escapedStr);
+            }
+
+            [TestMethod]
+            public void EscapesComma() {
+                string str = "foo bar, comma";
+                string escapedStr = new CsvHelper().Escape(str);
+                string expectedStr = "\"foo bar, comma\"";
+                Assert.AreEqual(expectedStr, escapedStr);
+            }
+            [TestMethod]
+            public void EscapesQuotes() {
+                string str = "foo bar\"double";
+                string escapedStr = new CsvHelper().Escape(str);
+                string expectedStr = "\"foo bar\"\"double\"";
+                Assert.AreEqual(expectedStr,escapedStr);
+            }
+        }
+    }
+}
