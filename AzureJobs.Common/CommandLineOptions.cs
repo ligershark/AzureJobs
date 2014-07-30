38584cb256003a4fce31638b22c334442d83a32e
Phil Campbell <philoushka@gmail.com>
2014-07-30 09:27:39 -0700

    move cmdline aliases to attributes. use Reflection to validate switches given by user

--- AzureJobs.Common/CommandLineOptions.cs
+++ AzureJobs.Common/CommandLineOptions.cs
@@ -1,22 +1,64 @@
-﻿namespace AzureJobs.Common {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+namespace AzureJobs.Common {
     public class CommandLineOptions {
-        public static readonly string OptimizerCacheFileArg = "--logfile";
-        public static readonly string ColorArg = "--color";
-        public static readonly string ItemsToProcessDirectoryArg = "--folder";
-        public static readonly string NameArg = "--name";
-        public static readonly string StartListenerArg = "--startlistener";
-        public static readonly string HelpArg = "--help";
-        public static readonly string HelpShortArg = "/?";
-        public static readonly string ForceArg = "--force";
-        public static readonly string SuppressCsvReportArg = "NoReport";
-
+        [CommandLineArg(Alias = "--name", ShortAlias = "/n", Description = @"TBD")]
         public string Name { get; set; }
+
+        [CommandLineArg(Alias = "--color", ShortAlias = "/c", Description = @"TBD")]
         public string Color { get; set; }
+
+        [CommandLineArg(Alias = "--dir", ShortAlias = "/d", IsRequired = true, Description = @"The directory containing files you want to compress. Subdirectories will be included.")]
         public string ItemsToProcessDirectory { get; set; }
+
+        [CommandLineArg(Alias = "--noreport", ShortAlias = "/nr", Description = @"Won't output a .csv file with compression results.")]
         public bool SuppressCsvReport { get; set; }
+
+        [CommandLineArg(Alias = "--startlistener", ShortAlias = "/sl", Description = @"TBD")]
         public bool? StartListener { get; set; }
-        public bool? DisplayHelp { get; set; }
+
+        [CommandLineArg(Alias = "--help", ShortAlias = "/?", Description = @"Displays this text.")]
+        public bool DisplayHelp { get; set; }
+
+        [CommandLineArg(Alias = "--force", ShortAlias = "/fo", Description = @"Force the optimizer to recompress any files it's marked as processed in a previous run.")]
         public bool ShouldForceOptimize { get; set; }
+
+        [CommandLineArg(Alias = "--cache", ShortAlias = "/ca", Description = @"Specify the file to keep working set of files. If unspecified, then %APPDATA%\LigerShark\....")]
         public string OptimizerCacheFile { get; set; }
+
+        /// <summary>
+        /// Build the Help display for console users. Grab all info from the attributes and format nicely for display.
+        /// </summary>        
+        public override string ToString() {
+            const string ArgDisplay = "{0} or {1} \t: {2} {3}";
+            var helpText = new StringBuilder("Accepted Args");
+            helpText.AppendLine();
+
+            foreach (var cmdLineAttr in ListAllCommandLineArgs().OrderByDescending(x => x.IsRequired).ThenBy(x => x.Alias)) {
+                string argHelp = string.Format(ArgDisplay,
+                    cmdLineAttr.ShortAlias,
+                    cmdLineAttr.Alias,
+                    cmdLineAttr.Description,
+                    cmdLineAttr.IsRequired ? "**Required.**" : string.Empty);
+                helpText.AppendLine(argHelp);
+
+            }
+            return helpText.ToString();
+        }
+        private IEnumerable<CommandLineArgAttribute> ListAllCommandLineArgs() {
+            foreach (PropertyInfo prop in typeof(CommandLineOptions).GetProperties()) {
+                yield return prop.GetCustomAttributes(typeof(CommandLineArgAttribute)).OfType<CommandLineArgAttribute>().FirstOrDefault();
+            }
+        }
+    }
+
+    public class CommandLineArgAttribute : Attribute {
+        public string Alias { get; set; }
+        public string ShortAlias { get; set; }
+        public string Description { get; set; }
+        public bool IsRequired { get; set; }
     }
 }
